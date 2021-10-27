71ad5c89ed259b72c0536bcd7f93daeb16f53a36
DAMAR2401 <93277682+DAMAR2401@users.noreply.github.com>
2021-10-27 23:29:29 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import time
-
-for i in range(5000):
-    time.sleep(1)
-    cords=pyautogui.locateOnScreen("Capture.PNG")
-    if cords != None:
-      print(pyautogui.locateOnScreen("Capture.PNG"))
-      pyautogui.leftClick(cords)

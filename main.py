fa1f5bb4abb8704da67588c01830bf86b752e5fe
DAMAR2401 <93277682+DAMAR2401@users.noreply.github.com>
2021-10-28 21:24:13 +0300

    BOT_Hazır

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import time
-
 import pyautogui
-#for i in range(5000): bundan sonra time.sleep() komutu olacak - x = 1 while x > 0: bu ikincikod
+from win10toast import ToastNotifier
+
 x = 1
 while x > 0:
     time.sleep(5)
-    cords=pyautogui.locateOnScreen("Capture.PNG", confidence=0.9)
-    if cords != None:
-      print(cords)
-      from win10toast import ToastNotifier
-
-      toaster = ToastNotifier()
-      toaster.show_toast("ADUZEM!!!",
-                         "Bot hemen başlıyor",
-                         duration=4)
-      pyautogui.leftClick(cords)
-
-
-
-
+    cords = pyautogui.locateOnScreen("Capture.PNG", confidence=0.9)
+    if cords is not None:
+        print(cords)
+        toaster = ToastNotifier()
+        toaster.show_toast("ADUZEM!!!", "Bot hemen başlıyor", duration=4)
+        pyautogui.leftClick(cords)
